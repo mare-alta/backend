a50521bec80e02611854fe3fe805bd4b13f0918b
thalesgibbon <thales.gibbon@gmail.com>
2019-12-08 06:54:24 -0300

    add cors

--- api/settings.py
+++ api/settings.py
@@ -122,5 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL = True
+
 import django_heroku
 django_heroku.settings(locals())
